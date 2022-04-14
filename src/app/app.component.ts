c605c9dbd8ae47b79c4a267717433ce355d70a5b
Rory Skipper <roryskipper123@gmail.com>
2022-04-14 12:59:10 -0600

    Ensure HttpService is created only once + removed UserType from User

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 import { PrimeNGConfig } from 'primeng/api';
+import { HttpService } from './services/http.service';
 import { UserService } from './services/user.service';
 
 @Component({
@@ -17,7 +18,8 @@ export class AppComponent implements OnInit {
   constructor(
     private primengConfig: PrimeNGConfig,
     private router: Router,
-    private userService: UserService
+    private userService: UserService,
+    private httpService: HttpService
   ) {
     this.router.events.subscribe((val) => {
       if (val instanceof NavigationEnd) {
--- src/app/objects/user.ts
+++ src/app/objects/user.ts
@@ -1,13 +1,7 @@
-export enum UserType {
-  EMPLOYEE = 'Employee',
-  MANAGER = 'Manager',
-}
-
 export interface User {
   username: string;
   firstName: string;
   lastName: string;
   email: string;
   dateCreated: Date;
-  userType: UserType;
 }
